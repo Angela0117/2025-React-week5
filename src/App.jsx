21bc318ccdb068681340ac10e8d5b40751c5b479
Angela <angela.huang0117@gmail.com>
2025-03-16 00:59:12 +0800

    刪除購物車項目（單一、全部）、調整購物車產品數量功能完成

--- src/App.jsx
+++ src/App.jsx
@@ -68,9 +68,44 @@ function App() {
           qty:Number(qty)
         }
       })
-     getCart();//加入購物車後重新取得列表
+     getCart();//每次加入購物車後重新取得列表
     } catch (error) {
     alert("加入購物車失敗")
+    }
+  }
+
+  //清空購物車(全部)
+  const removeCart = async()=>{
+    try {
+      await axios.delete(`${BASE_URL}/v2/api/${API_PATH}/carts`)
+     getCart();//每次加入購物車後重新取得列表
+    } catch (error) {
+    alert("清空購物車失敗")
+    }
+  }
+
+   //刪除購物車(單一)
+   const removeCartItem = async(cartItem_id)=>{
+    try {
+      await axios.delete(`${BASE_URL}/v2/api/${API_PATH}/cart/${cartItem_id}`)
+     getCart();//每次加入購物車後重新取得列表
+    } catch (error) {
+    alert("刪除購物車品項失敗")
+    }
+  }
+
+  //調整購物車產品數量 (六角API參數有三個，所以都代進去)
+  const updateCartItem = async(cartItem_id,product_id,qty)=>{
+    try {
+      await axios.put(`${BASE_URL}/v2/api/${API_PATH}/cart/${cartItem_id}`,{
+        data:{
+          product_id,
+          qty:Number(qty)
+        }
+      })
+     getCart();//每次加入購物車後重新取得列表
+    } catch (error) {
+    alert("更新購物車品項失敗")
     }
   }
 
@@ -180,71 +215,80 @@ function App() {
             </div>
           </div>
         </div>
+       
+        {/* 若購物車長度大於0，則顯示以下資料*/}
+        {cart.carts?.length > 0 && (
+          <div>
+            <div className="text-end py-3">
+              <button onClick={removeCart} className="btn btn-outline-danger" type="button">
+                清空購物車
+              </button>
+            </div>
 
-        <div className="text-end py-3">
-          <button className="btn btn-outline-danger" type="button">
-            清空購物車
-          </button>
-        </div>
+            <table className="table align-middle">
+              <thead>
+                <tr>
+                  <th></th>
+                  <th>品名</th>
+                  <th style={{ width: "150px" }}>數量/單位</th>
+                  <th className="text-end">單價</th>
+                </tr>
+              </thead>
 
-        <table className="table align-middle">
-          <thead>
-            <tr>
-              <th></th>
-              <th>品名</th>
-              <th style={{ width: "150px" }}>數量/單位</th>
-              <th className="text-end">單價</th>
-            </tr>
-          </thead>
-
-          <tbody>
-            {}
-            {cart.carts?.map((cartItem) => (
-            <tr key={cartItem.id}>
-              <td>
-                <button type="button" className="btn btn-outline-danger btn-sm">
-                  x
-                </button>
-              </td>
-              <td>{cartItem.product.title}</td>
-              <td style={{ width: "150px" }}>
-                <div className="d-flex align-items-center">
-                  <div className="btn-group me-2" role="group">
-                    <button
-                      type="button"
-                      className="btn btn-outline-dark btn-sm"
-                    >
-                      -
+              <tbody>
+                {/* */}
+                {cart.carts?.map((cartItem) => (
+                <tr key={cartItem.id}>
+                  <td>
+                    <button onClick={()=>removeCartItem(cartItem.id)} type="button" className="btn btn-outline-danger btn-sm">
+                      x
                     </button>
-                    <span
-                      className="btn border border-dark"
-                      style={{ width: "50px", cursor: "auto" }}
-                    >{cartItem.qty}</span>
-                    <button
-                      type="button"
-                      className="btn btn-outline-dark btn-sm"
-                    >
-                      +
-                    </button>
-                  </div>
-                  <span className="input-group-text bg-transparent border-0">
-                  {cartItem.product.unit}
-                  </span>
-                </div>
-              </td>
-              <td className="text-end">{cartItem.total}</td>
-            </tr>
-            ))}
-          </tbody>
-          <tfoot>
-            <tr>
-              <td colSpan="3" className="text-end">
-                總計：
-              </td>
-              <td className="text-end" style={{ width: "130px" }}>{cart.total}</td>
-            </tr>
-          </tfoot>
-        </table>
+                  </td>
+                  <td>{cartItem.product.title}</td>
+                  <td style={{ width: "150px" }}>
+                    <div className="d-flex align-items-center">
+                      <div className="btn-group me-2" role="group">
+                        <button
+                        onClick={()=>updateCartItem(cartItem.id,cartItem.product.id,cartItem.qty-1)}
+                       //若數量為1，則disabled，才不會出現按下減鈕時數量為0
+                        disabled={cartItem.qty === 1}
+                          type="button"
+                          className="btn btn-outline-dark btn-sm"
+                        >
+                          -
+                        </button>
+                        <span
+                          className="btn border border-dark"
+                          style={{ width: "50px", cursor: "auto" }}
+                        >{cartItem.qty}</span>
+                        <button
+                        onClick={()=>updateCartItem(cartItem.id,cartItem.product.id,cartItem.qty+1)}
+                          type="button"
+                          className="btn btn-outline-dark btn-sm"
+                        >
+                          +
+                        </button>
+                      </div>
+                      <span className="input-group-text bg-transparent border-0">
+                      {cartItem.product.unit}
+                      </span>
+                    </div>
+                  </td>
+                  <td className="text-end">{cartItem.total}</td>
+                </tr>
+                ))}
+              </tbody>
+              <tfoot>
+                <tr>
+                  <td colSpan="3" className="text-end">
+                    總計：
+                  </td>
+                  <td className="text-end" style={{ width: "130px" }}>{cart.total}</td>
+                </tr>
+              </tfoot>
+            </table>
+          </div>
+        )}
       </div>
 
       <div className="my-5 row justify-content-center">
